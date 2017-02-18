7e7fe2de83f0d7c46fd03938b9a9aad3809bdac1
Matt Kearney <kearney_@hotmail.com>
2017-02-17 16:37:36 -0800

    Added timestamp to posts

--- DrawNShare/DrawNShare/MainHub.cs
+++ DrawNShare/DrawNShare/MainHub.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,8 +10,10 @@ namespace DrawNShare
     {
         public void Send(string name, string image)
         {
+            DateTime time = DateTime.Now;
+            string timestamp = time.ToString("MM/dd/yyyy HH:mm:ss");
             
-            Clients.All.broadcastMessage(name, image);
+            Clients.All.broadcastMessage(name, image, timestamp);
         }
     }
 }
