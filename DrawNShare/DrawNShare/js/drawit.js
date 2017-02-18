9cbc2b86c0cfdd742b057714db93f893f433fa18
Matt Kearney <kearney_@hotmail.com>
2017-02-17 16:35:39 -0800

    Added new tool options

--- DrawNShare/DrawNShare/js/drawit.js
+++ DrawNShare/DrawNShare/js/drawit.js
@@ -1,28 +1,51 @@
-var colors = {
-    palette: ['#ff0099', '#f3f315', '#83f52c', '#ff6600', '#6e0dd0', '#ffffff'],
-    selected: null
+// options for the drawing tools, such as stroke width and colours.
+var toolSettings = {
+    stroke: {
+        symbol: ['❘', '❙', '❚'],
+        values: [1 , 3, 5],
+        selected: 1
+    },
+    colors : {
+        palette: ['#ff0099', '#f3f315', '#83f52c', '#ff6600', '#6e0dd0', '#ffffff'],
+        selected: null
+    }
 }
 
-//var mainCanvas = null;
+
 
 var penTool = new Tool();
 
 $(document).ready(function () {
-    //mainCanvas = paper.getPaper();
+    // setup of the colour palettes
     var colorPaletteBar = ""
-    for (i = 0; i < colors.palette.length; i++) {
-        colorPaletteBar += '<button type="button" class="paletteColor" value="' + colors.palette[i] + '" style="background-color:' + colors.palette[i] + '"></button>'
+    for (i = 0; i < toolSettings.colors.palette.length; i++) {
+        colorPaletteBar += '<button type="button" class="paletteColor" value="' + toolSettings.colors.palette[i] + '" style="background-color:' + toolSettings.colors.palette[i] + '"></button>'
     }
     $('#colorPalette').append(colorPaletteBar);
-    colors.selected = colors.palette[0];
+    toolSettings.colors.selected = toolSettings.colors.palette[0];
+
+    // setup the width selector
+    var strokeSelector = ""
+    for (i = 0; i < toolSettings.stroke.symbol.length; i++) {
+        strokeSelector += '<button type="button" class="strokeWidth" value="' + toolSettings.stroke.values[i] + '">' + toolSettings.stroke.symbol[i] + '</button>'
+    }
+    $('#strokeSelector').append(strokeSelector);
+    
+
     penTool.activate();
 });
 
 // sets draw color with user clicks a palette option
 $('.paletteColor').click(function () {
-    colors.selected = $(this).val();
+    toolSettings.colors.selected = $(this).val();
     $('paletteColor').removeClass('selected');
     $(this).addClass('selected');
+});
+
+// sets stroke width when stroke selection is made
+$('.strokeWidth').click(function () {
+    toolSettings.stroke.selected = $(this).val();
+   
 });
 
 // clear button function
@@ -36,7 +59,7 @@ var path;
 penTool.minDistance = 10;
 penTool.onMouseDown = function (event) {
  	path = new Path();
- 	path.strokeColor = colors.selected;
+ 	path.strokeColor = toolSettings.colors.selected;
  }
 
 penTool.onMouseDrag = function(event) {
@@ -46,7 +69,7 @@ penTool.onMouseDrag = function(event) {
 
 penTool.onMouseUp = function(event) {
     path.add(event.point);
- 	path.strokeWidth = 5;
+ 	path.strokeWidth = toolSettings.stroke.selected;
  	path.strokeCap = 'round';
  	path.closed = false;
  	path.simplify(10);
